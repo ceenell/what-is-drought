5ab27be675a0dcbca5b0371ef4de2c4b4e306b05
collnell <cnell@usgs.gov>
2022-07-22 10:58:02 -0500

    add regions

--- 2_process.R
+++ 2_process.R
@@ -2,25 +2,38 @@ source('2_process/src/prep_stripswarm.R')
 
 p2_targets <- list(
   ##### General metadata #####
+  tar_target(p2_casc_list,
+             # Climate Adaptation Regions
+             list(NW = c('WA','OR','ID'),
+                  SW = c('CA','UT','NV','AZ'),
+                  SC = c('NM', 'TX', 'OK','LA'),
+                  NC = c('MT','ND','SD','WY','CO','NE','KS'),
+                  MW = c('WI','MN','IA','IN','IL','OH','MI','MO'),
+                  NE = c('ME','VT','NH','NY','NJ','MA','RI','CT','WV','VA','MD','DE','KY', 'DC', 'PA'),
+                  SE = c('AR','MS','TN','NC','SC','AL','FL','GA','PR'),
+                  PI = c('HI','AS','GU'),
+                  AK = c('AK'))
+  ),
   tar_target(p2_metadata,
-             readr::read_csv(p1_metadata_csv, col_types=cols())),
+             readr::read_csv(p1_metadata_csv, col_types=cols()) %>%
+               mutate(CASC = case_when(
+                 STATE %in% p2_casc_list$NW ~ 'NW',
+                 STATE %in% p2_casc_list$SW ~ 'SW',
+                 STATE %in% p2_casc_list$SC ~ 'SC',
+                 STATE %in% p2_casc_list$NC ~ 'NC',
+                 STATE %in% p2_casc_list$MW ~ 'MW',
+                 STATE %in% p2_casc_list$NE ~ 'NE',
+                 STATE %in% p2_casc_list$SE ~ 'SE',
+                 STATE %in% p2_casc_list$PI ~ 'PI',
+                 STATE %in% p2_casc_list$AK ~ 'AK',
+               ))
+             ),
 
   ##### Data for 1951-2020 #####
 
   ###### Get 1951-2020 metadata ######
   tar_target(p2_1951_2020_metadata,
              filter(p2_metadata, national_1951)),
-  tar_target(p2_casc_list,
-             # Climate Adaptation Regions
-             list(NW = c('WA','OR','ID'),
-                  SW = c('CA','UT','NV','NM'),
-                  NC = c('MT','ND','SD','WY','CO','NE','KS'),
-                  MW = c('WI','MN','IA','IN','IL','OH','MI'),
-                  NE = c('ME','VT','NH','NY','NJ','MA','RI','CT','WV','VA','MD','DE','KY'),
-                  SE = c('AR','MS','TN','NC','SC','AL','FL','GA','PR'),
-                  PI = c('HI','AS','GU'),
-                  AK = c('AK'))
-             ),
   
   ###### Load drought summaries ######
   tar_target(p2_1951_2020_drought_summary_site,
@@ -37,19 +50,12 @@ p2_targets <- list(
              readr::read_csv(p1_1951_2020_drought_prop_site_csv, col_types = cols()) %>%
               mutate(across(c(start, end), ~as.Date(.x, '%Y-%m-%d')))
              ),
-  tar_target(p2_site_prop_10,
-             # Filter to 10 threshold
-             p2_1951_2020_drought_summary_jd %>%
-               filter(threshold == 10) %>%
-               left_join(p2_1951_2020_metadata %>%
-                           select(StaID:STATE, HCDN_2009))
-             ),
   tar_target(p2_site_prop_2,
              # Filter to 10 threshold
              p2_1951_2020_drought_prop_jd_7d %>%
                filter(threshold == 2) %>%
                left_join(p2_1951_2020_metadata %>%
-                           select(StaID:STATE, HCDN_2009))
+                           select(StaID:STATE, HCDN_2009, CASC)) 
   ),
   tar_target(p2_1951_2020_drought_prop_jd,
              readr::read_csv(p1_1951_2020_drought_prop_jd_csv, col_types = cols())),
@@ -58,6 +64,7 @@ p2_targets <- list(
   tar_target(p2_1951_2020_drought_prop_jd_30d,
              readr::read_csv(p1_1951_2020_drought_prop_jd_30d_csv, col_types = cols())),
   tar_target(p2_site_swarm,
+             # 
              create_event_swarm(event_data = p2_site_prop_2,
                                 start_period = as.Date('2010-01-01'),
                                 end_period = as.Date('2020-12-31'))),
