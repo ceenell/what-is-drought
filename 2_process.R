1379de7d2324e14a208e445d25a491c99dc07240
Cee Nell <cnell@usgs.gov>
2022-08-05 15:31:20 -0500

    prep drought prop data for duration chart

--- 2_process.R
+++ 2_process.R
@@ -9,8 +9,6 @@ p2_targets <- list(
   ##### Data for 1951-2020 #####
 
   ###### Get 1951-2020 metadata ######
-  tar_target(p2_1951_2020_metadata,
-             filter(p2_metadata, national_1951)),
   tar_target(p2_casc_list,
              # Climate Adaptation Regions
              list(NW = c('WA','OR','ID'),
@@ -22,6 +20,22 @@ p2_targets <- list(
                   PI = c('HI','AS','GU'),
                   AK = c('AK'))
              ),
+  tar_target(p2_1951_2020_metadata,
+             filter(p2_metadata, national_1951)%>%
+               mutate(CASC = case_when(
+                 STATE %in% p2_casc_list$NW ~ 'NW',
+                 STATE %in% p2_casc_list$SW ~ 'SW',
+                 STATE %in% p2_casc_list$SC ~ 'SC',
+                 STATE %in% p2_casc_list$NC ~ 'NC',
+                 STATE %in% p2_casc_list$MW ~ 'MW',
+                 STATE %in% p2_casc_list$NE ~ 'NE',
+                 STATE %in% p2_casc_list$SE ~ 'SE'
+               )) %>%
+               filter(!is.na(CASC))),
+  tar_target(
+    p2_casc,
+    unique(p2_1951_2020_metadata$CASC)
+  ),
   
   ###### Load drought summaries ######
   tar_target(p2_1951_2020_drought_summary_site,
@@ -38,20 +52,6 @@ p2_targets <- list(
              readr::read_csv(p1_1951_2020_drought_prop_site_csv, col_types = cols()) %>%
               mutate(across(c(start, end), ~as.Date(.x, '%Y-%m-%d')))
              ),
-  tar_target(p2_site_prop_10,
-             # Filter to 10 threshold
-             p2_1951_2020_drought_summary_jd %>%
-               filter(threshold == 10) %>%
-               left_join(p2_1951_2020_metadata %>%
-                           select(StaID:STATE, HCDN_2009))
-             ),
-  tar_target(p2_site_prop_2,
-             # Filter to 2 threshold
-             p2_1951_2020_drought_prop_jd_7d %>%
-               filter(threshold == 2) %>%
-               left_join(p2_1951_2020_metadata %>%
-                           select(StaID:STATE, HCDN_2009))
-  ),
   tar_target(p2_1951_2020_drought_prop_jd,
              readr::read_csv(p1_1951_2020_drought_prop_jd_csv, col_types = cols())),
   tar_target(p2_1951_2020_drought_prop_jd_7d,
@@ -60,9 +60,22 @@ p2_targets <- list(
              readr::read_csv(p1_1951_2020_drought_prop_jd_30d_csv, col_types = cols())),
   tar_target(p2_site_swarm,
              create_event_swarm(event_data = p2_site_prop_2,
-                                start_period = as.Date('2010-01-01'),
+                                start_period = as.Date('2000-01-01'),
                                 end_period = as.Date('2020-12-31'))),
-
+  
+  ## Prep drought properties for "strip swarm" chart
+  tar_target(p2_site_prop_2,
+             # Filter to 2 threshold for now
+             p2_1951_2020_drought_prop_site %>%
+               filter(threshold == 2) %>%
+               left_join(p2_1951_2020_metadata %>%
+                           select(StaID:STATE, HCDN_2009, CASC))
+  ),
+  tar_target(p2_site_swarm_80s,
+             create_event_swarm(event_data = p2_site_prop_2,
+                                start_period = as.Date('1980-01-01'),
+                                end_period = as.Date('1999-12-31'))),
+  
   ###### Load annual stats ######
   tar_target(p2_1951_2020_annual_stats_site,
              readr::read_csv(p1_1951_2020_annual_stats_site_csv, col_types = cols())),
